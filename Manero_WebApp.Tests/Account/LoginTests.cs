34e8552702433b9eff3b431b3c7cc0caae3e1032
MalteNord <malte.nordstrand@gmail.com>
2024-06-02 15:23:10 +0200

    Removed Service

--- Manero_WebApp.Tests/Account/LoginTests.cs
+++ Manero_WebApp.Tests/Account/LoginTests.cs
@@ -3,46 +3,54 @@ using Microsoft.AspNetCore.Identity;
 using Moq;
 using Xunit;
 using Manero_WebApp.Services;
-using Microsoft.AspNetCore.Components;
 using Manero_WebApp.Data;
 using Microsoft.AspNetCore.Http;
 
-public class LoginServiceTests
+namespace YourProject.Tests
 {
-    [Fact]
-    public async Task LoginUser_SuccessfulLogin_RedirectsToProductHome()
+    public class LoginServiceTests
     {
-        // Arrange
-        var mockUserManager = new Mock<UserManager<ApplicationUser>>(
-            Mock.Of<IUserStore<ApplicationUser>>(), null, null, null, null, null, null, null, null);
-
-        var mockSignInManager = new Mock<SignInManager<ApplicationUser>>(
-            mockUserManager.Object,
-            Mock.Of<IHttpContextAccessor>(),
-            Mock.Of<IUserClaimsPrincipalFactory<ApplicationUser>>(),
-            null, null, null, null);
-
-        var mockNavigationManager = new Mock<NavigationManager>(MockBehavior.Loose, null);
-        var mockRedirectManager = new Mock<IdentityRedirectManager>(mockNavigationManager.Object);
-
-        mockSignInManager
-            .Setup(m => m.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
-            .ReturnsAsync(SignInResult.Success);
-
-        var loginService = new LoginService(mockSignInManager.Object, mockRedirectManager.Object);
-
-        var loginInput = new LoginService.InputModel
+        [Fact]
+        public async Task LoginUser_SuccessfulLogin_SignInSuccessful()
         {
-            Email = "test@example.com",
-            Password = "password",
-            RememberMe = true
-        };
-        loginService.Input = loginInput;
+            // Arrange
+            var mockUserManager = new Mock<UserManager<ApplicationUser>>(
+                Mock.Of<IUserStore<ApplicationUser>>(), null, null, null, null, null, null, null, null);
 
-        // Act
-        await loginService.LoginUser();
+            var mockSignInManager = new Mock<SignInManager<ApplicationUser>>(
+                mockUserManager.Object,
+                Mock.Of<IHttpContextAccessor>(),
+                Mock.Of<IUserClaimsPrincipalFactory<ApplicationUser>>(),
+                null, null, null, null);
 
-        // Assert
-        mockRedirectManager.Verify(redirect => redirect.RedirectTo("/producthome"), Times.Once);
+            // Mock the PasswordSignInAsync to return Success
+            mockSignInManager
+                .Setup(m => m.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
+                .ReturnsAsync(SignInResult.Success);
+
+            var loginService = new LoginService(mockSignInManager.Object); // pass null for RedirectManager
+
+            var loginInput = new LoginService.InputModel
+            {
+                Email = "test@example.com",
+                Password = "password",
+                RememberMe = true
+            };
+            loginService.Input = loginInput;
+
+            // Act
+            await loginService.LoginUser();
+
+            // Assert
+            mockSignInManager.Verify(m => m.PasswordSignInAsync(
+                It.Is<string>(email => email == loginInput.Email),
+                It.Is<string>(password => password == loginInput.Password),
+                It.Is<bool>(rememberMe => rememberMe == loginInput.RememberMe),
+                It.IsAny<bool>()),
+                Times.Once);
+
+            // Verify the result (though this might be done inside LoginService)
+            // Here, we're just verifying that PasswordSignInAsync was called with expected parameters.
+        }
     }
 }
